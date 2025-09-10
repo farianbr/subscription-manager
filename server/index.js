da743a4f3f1040892f1662f4da3d811992eede47
Farian Bin Rahman <farianrahman1000@gmail.com>
2025-09-10 23:10:20 +0600

    Add MongoDB integration and user/transaction models
    Introduced MongoDB connection logic and added Mongoose models for User and Transaction. Updated server setup to use Express 5 integration with Apollo Server, loaded environment variables, and added .env to .gitignore. Enhanced user resolver with a user query by ID.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
+import http from "http";
+import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv"
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from '@as-integrations/express5';
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import {connectDB} from "./db/connectDB.js"
+
+dotenv.config()
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`Server running at ${url}`);
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB()
+
+console.log("Server ready at http://localhost:4000/");

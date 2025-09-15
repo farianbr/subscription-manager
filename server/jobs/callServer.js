c093605951498ec9b59b2c0b6fe4f658f7871664
Farian Bin Rahman <farianrahman1000@gmail.com>
2025-09-15 21:22:23 +0600

    bug fix 2

--- server/jobs/callServer.js
+++ server/jobs/callServer.js
@@ -3,18 +3,22 @@ import https from "https";
 
 const URL = "https://graphql-crash-course.onrender.com";
 
-const callServer = new cron.CronJob("*/14 * * * *", function () {
-	https
-		.get(URL, (res) => {
-			if (res.statusCode === 200) {
-				console.log("GET request sent successfully");
-			} else {
-				console.log("GET request failed", res.statusCode);
-			}
-		})
-		.on("error", (e) => {
-			console.error("Error while sending request", e);
-		});
-});
+const callServer = new cron.schedule(
+  "*/14 * * * *",
+  function () {
+    https
+      .get(URL, (res) => {
+        if (res.statusCode === 200) {
+          console.log("GET request sent successfully");
+        } else {
+          console.log("GET request failed", res.statusCode);
+        }
+      })
+      .on("error", (e) => {
+        console.error("Error while sending request", e);
+      });
+  },
+  { timezone: "Asia/Dhaka" }
+);
 
 export default callServer;
